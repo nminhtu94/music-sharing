4d6e7e59aad435d99cfb946af9e80b43e3c18a84
nminhtu94 <nminhtu94@gmail.com>
2015-12-13 18:32:26 +0700

    Git ignore

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("[*] Music Sharing");
-        MusicSharingForm app = new MusicSharingForm();
-        NavigationController.getInstance().pushFrame(app);
-    }
-}
